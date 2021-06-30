7375b63c355037b0067970e51d1516b320cfd0cf
Mayuran <Mayuran@localhost>
2021-06-30 20:59:31 +0530

    Added a Eureka service which does the service discovery.

---
+++ Discovery-service/src/test/java/com/microservices/eurekaserver/Discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.eurekaserver.Discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
